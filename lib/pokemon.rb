c3c0ee672c77a00f0b0fbb84f2e97e6b546a5b50
Ryan DeLong <rdelong111@gmail.com>
2022-08-02 18:18:38 -0500

    commit tree 9c652fdc10cbb2d7d7a30e2e41c369a3b4f539c3

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+    db.execute(sql, name, type)
+  end
 end
